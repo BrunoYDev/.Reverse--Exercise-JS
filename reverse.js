c1c41b27f94bbcb0db2b074f4cd0f6d42af0a698
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-08-04 17:23:42 -0300

    Add: a array mock, to prevent the function to change the original array

--- reverse.js
+++ reverse.js
@@ -1,17 +1,26 @@
 function reverse(array){ // Receive an array
+    
+    if(!Array.isArray(array)){ // Verify if the requested parameter is an actual array
+        return 'Not array!';
+    }
+
+    let newArray = [...array]; // Use the spread operator to copy all the array values into another array so that the function doesn’t modify the original array.
+
     let left = 0; // Declare the left variable
-    let right = array.length - 1; // Declare the right variable, which is the last item in the array
+    let right = newArray.length - 1; // Declare the right variable, which is the last item in the array
 
     while(left < right){ // While the left variable value is less than the right variable value
-        let temp = array[left]; // Create a temp variable that stores the value of the array at the left position
-        array[left] = array[right]; // Change the value of the array at the left position to the value at the right position
-        array[right] = temp; // Change the value of the array at the right position to the value stored in the temp variable
+        let temp = newArray[left]; // Create a temp variable that stores the value of the array at the left position
+        newArray[left] = newArray[right]; // Change the value of the array at the left position to the value at the right position
+        newArray[right] = temp; // Change the value of the array at the right position to the value stored in the temp variable
 
         left++; // Increment left
         right--; // Decrement right
     } // This while loop will return to the start and revalidate if the left is still less than right
 
-    return array; // When the while loop ends, the function will return the reversed array
+    return newArray; // When the while loop ends, the function will return the reversed array
 }
+
+console.log(reverse(["b","o","l","o"]))
 
 module.exports = reverse;
